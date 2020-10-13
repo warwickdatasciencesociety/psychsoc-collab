019d28dd757c7257cbb8540ef9b6a7e8dcb39ba4
Bhav <bhavikmakwana09@gmail.com>
2020-10-13 13:46:53 +0100

    Hidden secret values

--- app/config.py
+++ app/config.py
@@ -1,12 +1,16 @@
 # config variables that eventually should be retrieved from a .env file
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = "super-secret"
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_ACCESS_COOKIE_PATH = "/api/"
     JWT_REFRESH_COOKIE_PATH = "/token/refresh"
     JWT_COOKIE_CSRF_PROTECT = False
-    JWT_SECRET_KEY = "super-secret"
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@db:3306/psych_collab"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('DB_PASSWORD')}@db:3306/psych_collab"
